75ddc038ba7f9a54c75cf44f61e44056b7596659
zuolizhu <zuolizhu5960x@gmail.com>
2020-12-06 18:21:52 -0500

    deploy test

--- src/API/loadJobs.js
+++ src/API/loadJobs.js
@@ -1,7 +1,7 @@
 export default async function loadJobs(page) {
   const pageId = parseInt(page) || 1
-  // const response = await fetch(`https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?page=${pageId}`)
-  const response = await fetch(`http://localhost:4004/jobs`)
+  const response = await fetch(`https://jobs.github.com/positions.json?page=${pageId}`)
+  // const response = await fetch(`http://localhost:4004/jobs`)
   const jobs = await response.json()
   const nextPage = jobs.length === 50 ? pageId + 1 : null
   const loadedData = { data: jobs, nextPage }
