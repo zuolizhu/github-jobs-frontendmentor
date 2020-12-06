d2f0f2dad4b34da99794bdb28b95b3d97f5891ca
zuolizhu <zuolizhu5960x@gmail.com>
2020-12-06 03:55:19 -0500

    job detail page content up

--- src/API/useJob.js
+++ src/API/useJob.js
@@ -1,7 +1,8 @@
 import { useQuery } from 'react-query'
 
 const getJobById = async (_, jobId) => {
-  const data = await fetch(`https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions/${jobId}.json`)
+  // const data = await fetch(`https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions/${jobId}.json`)
+  const data = await fetch(`http://localhost:4004/job`)
   return await data.json()
 }
 
--- src/Pages/Job.js
+++ src/Pages/Job.js
@@ -1,33 +1,82 @@
+import moment from 'moment'
+import { useRef } from 'react'
 import useJob from '../API/useJob'
+import ReactHtmlParser from 'react-html-parser'
 import { useParams, useHistory } from 'react-router-dom'
-
+import IconBack from '../Assets/desktop/arrow-left-solid.svg'
 
 const getDomain = (url) => (new URL(url)).hostname
+
+const transform = (node, index) => {
+  if (node.type === 'tag' && node.name=== 'p') {
+    node.attribs.class = 't-body'
+  }
+  if (node.type === 'tag' && node.name=== 'strong') {
+    // node.name = 'h3'
+    // node.attribs.class = 't-h3'
+    // console.log(node)
+  }
+}
+const parserOption = { transform }
 
 export default function Job() {
   const { id } = useParams()
   const history = useHistory()
+  const howToApplyRef = useRef()
   const handleBackClick = () => history.push('/')
   const { status, data, error, isFetching } = useJob(id);
-
+  const handleApplyNowClick = () => howToApplyRef.current.scrollIntoView({ behavior: 'smooth' })
+  
   return (
-    <div>
-      <div>
-        <button onClick={handleBackClick}>Back</button>
-      </div>
+    <main className="main main--job-detail">
       {status === "loading" ? (
         "Loading..."
       ) : status === "error" ? (
         <span>Error: {error.message}</span>
       ) : (
         <>
-          <h1>{data.id}</h1>
-          <div>
-            <p>{data.company}</p>
+          <div className="container">
+            <div className="max-width-container">
+              <button className="btn btn--back" onClick={handleBackClick}>
+                <span className="btn--back__icon">
+                  <img src={IconBack} alt="back arrow icon"/>
+                </span>
+                <span>Back</span>
+              </button>
+              <section className="company">
+                <div className="company__logo">
+                  <img src={data.company_logo} alt="company logo"/>
+                </div>
+                <div className="company__namesite">
+                  <h2>{data.company}</h2>
+                  <p className="t-light-gray t-body">{getDomain(data.company_url)}</p>
+                </div>
+                <a className="btn btn--companysite" href={data.company_url} rel="noreferrer" target="_blank">Company Site</a>
+              </section>
+              <section className="job-detail">
+                <div className="job-detail__heading">
+                  <div className="job-detail__timetype t-light-gray t-body">
+                    <p className="job-detail__time">{moment(new Date(data.created_at)).fromNow()}</p>
+                    <div className="spacer-dot"></div>
+                    <p className="job-detail__type">{data.type}</p>
+                  </div>
+                  <h1 className="job-detail__title">{data.title}</h1>
+                  <p className="t-location t-violet">{data.location}</p>
+                  <button className="btn btn--applynow" onClick={handleApplyNowClick}>Apply Now</button>
+                </div>
+                <div className="job-detail__description">{ReactHtmlParser(data.description, parserOption)}</div>
+                <div ref={howToApplyRef} className="job-detail__how-to-apply">{ReactHtmlParser(data.how_to_apply)}</div>
+              </section>
+              <div>{isFetching ? "Background Updating..." : " "}</div>
+            </div>
+            </div>
+          <div className="bottom-title">
+            <h2>{data.title}</h2>
+            <p className="t-light-gray t-body">{data.company}</p>
+            <button className="btn btn--applynow" onClick={handleApplyNowClick}>Apply Now</button>
           </div>
-          <div>{isFetching ? "Background Updating..." : " "}</div>
         </>
       )}
-    </div>
+    </main>
   );
 }
